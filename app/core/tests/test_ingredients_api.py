bd278a5e75d09cfc93086a12186edc9160f1e413
jn8366ew <jn8366ew@mnsu.edu>
2021-10-18 02:56:47 -0500

    Added ingredient features

--- app/core/tests/test_ingredients_api.py
+++ app/core/tests/test_ingredients_api.py
@@ -9,3 +9,56 @@ from core.models import Ingredient
 
 from recipe.serializers import IngredientSerializer
 
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientsApiTests(TestCase):
+    """Test INGREDIENTS api is available to public"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientApiTests(TestCase):
+    """Test INGREDIENTS api is available to registered users"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@test.com',
+            'testman'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        """Test retrieving a list of ingredients"""
+        Ingredient.objects.create(user=self.user, name='Lettuce')
+        Ingredient.objects.create(user=self.user, name='Salt')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test returned ingredients to authenticated users"""
+        user2 = get_user_model().objects.create_user(
+            'User2@test.com',
+            'User2'
+        )
+        Ingredient.objects.create(user=user2, name='Orange')
+        ingredient = Ingredient.objects.create(user=self.user, name='Pork')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
