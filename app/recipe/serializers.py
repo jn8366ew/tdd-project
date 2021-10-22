bceecf886359c9729a148ceb1ce4e1190e3ddcc8
jn8366ew <jn8366ew@mnsu.edu>
2021-10-22 08:55:34 -0500

    Code refactored, BaseRecipeAttrViewSet added

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -17,3 +17,22 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = ('id', 'name',)
         read_only_fields = ('id',)
+
+# class RecipeSerializer(serializers.ModelSerializer):
+#     """Serialize a recipe"""
+#     ingredients = serializers.PrimaryKeyRelatedField(
+#         many=True,
+#         queryset=Ingredient.objects.all()
+#
+#     )
+#     tags = serializers.PrimaryKeyRelatedField(
+#         many=True,
+#         queryset=Tag.objects.all()
+#     )
+#
+#     class Meta:
+#         model = Recipe
+#         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
+#                   'price', 'link', )
+#         read_only_fields = ('id', )
+
