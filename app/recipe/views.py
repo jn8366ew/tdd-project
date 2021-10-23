3b3d6ff092b949eac279622b6609ea064d767251
jn8366ew <jn8366ew@mnsu.edu>
2021-10-23 01:13:22 -0500

    added listing recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
@@ -34,13 +34,13 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     serializer_class = serializers.IngredientSerializer
 
 
-# class RecipeViewSet(viewsets.ModelViewSet):
-#     """Manage recipes in the database"""
-#     serializer_class =serializers.RecipeSerializer
-#     queryset = Recipe.objects.all()
-#     authentication_classes = (TokenAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def get_queryset(self):
-#         """Read recipes for authenticated users"""
-#         return self.queryset.filter(user=self.request.user)
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Read recipes for authenticated users"""
+        return self.queryset.filter(user=self.request.user)
